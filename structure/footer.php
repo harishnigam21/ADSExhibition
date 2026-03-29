bb50aefb2c0f0777f55371e46f748e73397ae39c
Harish Nigam <harishnigam21@gmail.com>
2026-03-29 14:44:14 +0530

    Add front-end assets and layout templates
    Import a large collection of static front-end assets: CSS (including plugins and pickup form styles), JS (plugins, validation, pickup, cubeportfolio), fonts, numerous images and media for gallery/clients/work, and a .DS_Store. Also add layout partials (structure/header.php, header2.php, footer.php, footer2.php, menu.php). These files provide the initial front-end resources and template fragments for the site.

---
+++ structure/footer.php
@@ -0,0 +1,84 @@
+<footer id="footer-option">
+    <style>
+        .whatsappicon:hover{
+            transition:all 1s ease;
+    transform: scale(1.25);
+}
+    </style>
+                <div class="container">
+                    <div class="row">
+                        
+                        <div class="col-lg-5 margin-btm-20">
+                            <div class="footer-col margin-btm-20">
+                                <h3>About Us</h3>
+                                <div class="tweet-desc">
+                                    ADS EXHIBITION is one of the best exhibition companies in Delhi, with over 10+ years of experience in creating exhibition stands that bring brands to life. As a trusted exhibition stand builder in Delhi NCR, our team handles everything from concept and design to production and installation, ensuring a smooth and stress-free process for our clients. With ADS EXHIBITION by your side, you can be confident that your exhibition presence will be memorable, professional, and built to impress.
+                                </div>
+                                <hr>
+                            </div>
+                        </div><!--footer col end-->
+                        <div class="col-lg-3 margin-btm-20">
+                            <div class="footer-col margin-btm-20">
+                                <h3>Follow us</h3>
+                                <ul class=" list-inline social-btn">
+                                    <li class="list-inline-item"><a href="https://www.facebook.com/adsexhibition.in/"><i class="ion-social-facebook" data-toggle="tooltip" data-placement="top" title="" data-original-title="Like On Facebook"></i></a></li>
+                                    <!--<li class="list-inline-item"><a href=""><i class="ion-social-twitter" data-toggle="tooltip" data-placement="top" title="" data-original-title="Follow On twitter"></i></a></li>-->
+                                    <li class="list-inline-item"><a href="https://wa.me/<?php echo $website['storeNumber1']; ?>"><i class="ion-social-whatsapp" data-toggle="tooltip" data-placement="top" title="" data-original-title="Chat on whatsApp"></i></a></li>
+                                    <li class="list-inline-item"><a href="https://www.linkedin.com/in/ads-exhibition-74027518a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app "><i class="ion-social-linkedin" data-toggle="tooltip" data-placement="top" title="" data-original-title="Follow On LinkedIn"></i></a></li>
+                                </ul>
+                            </div> 
+                        </div><!--footer col end-->
+                        <div class="col-lg-4 margin-btm-20">
+                            <div class="footer-col">
+                                <h3>contact info</h3>
+                                <p><i class="ion-home" style="padding-right:10px"></i>   <?php echo $website['storeAddress']; ?>.</p>
+                                <hr>
+                                <div style="display:flex;gap:10px;padding:10px 0;align-items:center;">
+                        <strong style="white-space:nowrap;">Warehouse :</strong>
+                        <p style="margin:0;"><span style="white-space:nowrap;">Delhi(NCR)</span>, <span style="white-space:nowrap;">Mumbai</span>, <span style="white-space:nowrap;">Bangalore</span>, <span style="white-space:nowrap;">Hyderabad</span></p>
+                    </div>
+                                <p><i class="ion-iphone" style="padding-right:10px"></i>   <a style="color:unset;" target="_blank" href="https://wa.me/<?php echo $website['storeNumber1']; ?>">+91 <?php echo $website['storeNumber1']; ?></a>, <a style="color:unset;" target="_blank" href="https://wa.me/<?php echo $website['storeNumber2']; ?>">+91 <?php echo $website['storeNumber2']; ?></a></p>
+                                <p><i class="ion-email" style="padding-right:10px"></i>   <a style="color:unset;" target="_blank" href="mailto:<?php echo $website['mailingEmails'];?>"><?php echo $website['mailingEmails'];?></a></p>
+                            </div>
+    
+                        </div><!--footer col end-->
+                    </div>
+                </div>
+                <div class="footer-copyright">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-2 footer-logo">
+                               
+                              
+                              <a class="navbar-brand" href="index.php" style="margin-right: -10%;">
+                     		<img src="assets/img/footer_logo.jpg" style="width: 50%;aspect-ratio: auto;" class="img-fluid logo-dark" alt=""> 
+                    </a>
+                            </div>
+                            <div class="col-md-5" style="display:flex;align-items:center;">
+                                <p style="padding:0;margin:0;">© Copyright <?php echo date('Y'); ?>.All right reserved</p>
+                            </div>
+                            <div class="col-md-5" style="display:flex;align-items:center;">
+                                <ul class="list-inline footer-nav">
+                                    <li class="list-inline-item"><a href="index.php">Home</a></li>
+                                    <li class="list-inline-item"><a href="services.php">Services</a></li>
+                                    <!--<li class="list-inline-item"><a href="innovations.php">Our Mission</a></li>-->
+                                    <li class="list-inline-item"><a href="about-us.php">About us</a></li>
+                                    <li class="list-inline-item"><a href="contact-us.php">Contact us</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a href="https://wa.me/<?php echo $website['storeNumber1']; ?>"><i class="fa fa-whatsapp whatsappicon" style="position:fixed;bottom:10px;right:10px;font-size:64px;color:green"></i><a/>
+        </footer>
+
+        <a href="#" class="scrollToTop"><i class="ion-android-arrow-dropup-circle"></i></a>
+        <!--back to top end-->
+
+        <!-- jQuery plugins. -->
+        <script src="assets/js/plugins/plugins.js"></script>
+        <script src="assets/js/app.js"></script>
+        <script src="assets/js/pickup.js"></script>
+
+    </body>
+</html>
